35e9bd2f2040b51b3b5bf1dfcd5463c8490f7722
Xenium <30338644+Frenzoid@users.noreply.github.com>
2019-01-18 03:49:33 +0100

    Create fileManager.ts

---
+++ src/utils/fileManager.ts
@@ -0,0 +1,46 @@
+import { UploadedFile } from 'express-fileupload';
+import * as mkdir from 'mkdirp';
+
+export class FileManager {
+
+  public static getExtension(expressFile: UploadedFile){
+    return `.${expressFile.name.split('.')[1]}`;
+  }
+
+  public static getSize(expressFile: UploadedFile){
+    const bytes = expressFile.data.byteLength;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes === 0) return 'n/a';
+    const div = Math.log(bytes) / Math.log(1024);
+    const i = parseInt(`${Math.floor(div)}`, 10);
+    if (i === 0) return `${bytes} ${sizes[i]})`;
+    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`;
+  }
+
+  public static checkMimetype(expressFile: UploadedFile, type: string){
+    return expressFile.mimetype.startsWith(type) ? true : false;
+  }
+
+  public static manageFile(expressFile: UploadedFile, movePath: string, newName: string = expressFile.name): Promise<string> {
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise((resolve, reject) => {
+      const completePath: string = movePath + newName;
+
+      mkdir(movePath, (errMkdir) => {
+        if (errMkdir) {
+          reject(`Error creating folder for the user's avatar: ${errMkdir}`);
+        }
+
+        expressFile.mv(completePath, (errMoving: string) => {
+          if (errMoving) {
+            reject(`Error moving file: ${errMoving}`);
+          }
+
+          const serverPath = completePath.split('public')[1];
+
+          resolve(serverPath);
+        });
+      });
+    });
+  }
+}
