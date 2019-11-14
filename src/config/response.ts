d185ceea32c109f9d21c9cf0caf410d7d1987941
Frenzoid <saassabau100@gmail.com>
2019-11-14 07:32:22 +0100

    documented a bit more the response class

--- src/config/response.ts
+++ src/config/response.ts
@@ -9,12 +9,19 @@ export class ResponseModel {
   public errors: { critical: boolean, messages: string[] };
   public timestamp: Date;
 
-  // Adds a warning, (http status 199).
+  /* Adds a warning, (proceed as http status 199).
+  Miscellaneous warning The warning text MAY include
+  arbitrary information to be presented to a human user, or logged.
+
+  A system receiving this warning MUST NOT take any automated action,
+  besides presenting the warning to the user.*/
   addWarning(msg: string): void {
     this.errors.messages.push(msg);
   }
 
-  // Adds a critical error, (http status 500).
+  /* Adds a critical error, (proceed as http status 500).
+  The server encountered an unexpected condition which prevented it from fulfilling the request,
+  in our case, an unrecoverable error (any throws being triggered during the process of the request). */
   addError(msg: string): void {
     this.errors.critical = true;
     this.errors.messages.push(msg);
